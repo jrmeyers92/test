61f41e610be97c8096ea38ea8dfc9bac15884078
Jake Meyers <JakeRMeyers92@gmail.com>
2022-02-11 13:01:25 -0600

    test

---
+++ class.js
@@ -0,0 +1,12 @@
+class Car {
+  constructor(make, model, year, color, mpg){
+     this.make = make,
+     this.model = model,
+     this.year = year,
+     this.color = color,
+     this.mpg = mpg
+  }
+}
+
+let myCar = new Car('Tesla', 'Model S', 2019)
+console.log(myCar)
